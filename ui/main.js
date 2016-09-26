6811405a89a6c121cc2473261e347e80a94fdc9b
kritixilithos <mohdmalik02@gmail.com>
2016-09-26 10:09:16 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@
 var button = document.getElementById("counter");
 var counter = 0;
 console.log("loaded");
-button.onClick = function() {
+button.onclick = function() {
     console.log("sd");
     
     counter++;
