d757ec6d155c0c3293b62a9497fc8a2cda3a633a
kritixilithos <mohdmalik02@gmail.com>
2016-09-28 11:28:27 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,26 @@
 //Counter button code
 var button = document.getElementById("counter");
-var counter = 0;
-console.log("loaded");
+
+
 button.onclick = function() {
     
-    counter++;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //create request object
+    var request = XMLHttpRequest();
+    
+    //Capture request and store in var
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if(request.status === 200) {
+                var counter = request.responsetext;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //Not done yet
+    };
+    
+    //make the request
+    request.open('GET', 'http://kritixilithos.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
