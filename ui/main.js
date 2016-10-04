342f1bb0239145d7b76a47b5a7f1a8eeffdc6428
kritixilithos <mohdmalik02@gmail.com>
2016-10-04 15:39:23 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,15 +31,31 @@ var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
     //Make request to server and send name
+        // Create a request object
+    var request = new XMLHttpRequest();
     
-    //Capture list of names and render as list
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
+                //Capture list of names and render as list
     
-    var names = ["name1", "name2", "name3"];
-    var list = "";
-    for(var i = 0; i < names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-    }
+                var names = request.responseText
+                names = JSON.parse(names);
+                var list = "";
+                for(var i = 0; i < names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                
+                var ul = document.getElementById("namelist");
+                ul.innerHTML = list;
+            }
+        }  
+      // Not done yet
+    };
     
-    var ul = document.getElementById("namelist");
-    ul.innerHTML = list;
+    // Make the request
+    request.open('GET', 'http://kritixilithos.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
 };
