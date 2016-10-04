94f83bad9a3d29bac51f80f8de844acc07795dc7
kritixilithos <mohdmalik02@gmail.com>
2016-10-04 15:30:38 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,9 +99,9 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) {// URL: /submit-name?name=XXXXXX
     //Get the name from the request object
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JSON JAVASCRIPT OBJECT NOTATION
